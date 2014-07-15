bcf03c0558fd1262aa5b01db57c0650aefbbb32f
paulgator <paulgator@pisem.net>
2014-07-15 13:05:56 +0300

    Update goldrush.js

--- goldrush.js
+++ goldrush.js
@@ -28,7 +28,7 @@ function getLongJumpValue(a,b) {
                     return ["left",left];
                     } else {
                         return [];
-                     }
+                        }
 }
 
 function getValuableTarget(a,s,e) {
@@ -36,8 +36,8 @@ function getValuableTarget(a,s,e) {
     var mv=0;
     var t=0;
         for (var i=0;i<a.length;i++) {
-        v=Math.pow(a[i].bountyGold,2)/Math.pow(getDistance(s.pos,a[i].pos),2);
-            if (v>mv && getDistance(a[i].pos,s.pos)<getDistance(a[i].pos,e.pos)-5) {
+            v=Math.pow(a[i].bountyGold,2)/Math.pow(getDistance(s.pos,a[i].pos),2);
+            if (v>mv && getDistance(a[i].pos,s.pos)<getDistance(a[i].pos,e.pos)) {
                 mv=v;
                 t=a[i];
             }
@@ -50,8 +50,8 @@ function getClosestTarget(a,s,e) {
     var mv=0;
     var t=0;
         for (var i=0;i<a.length;i++) {
-           v=Math.pow(a[i].bountyGold,1)/Math.pow(getDistance(s.pos,a[i].pos),2);
-           if (v>mv && getDistance(a[i].pos,s.pos)<getDistance(a[i].pos,e.pos)) {
+            v=Math.pow(a[i].bountyGold,1)/Math.pow(getDistance(s.pos,a[i].pos),2);
+            if (v>mv && getDistance(a[i].pos,s.pos)<getDistance(a[i].pos,e.pos)) {
                 mv=v;
                 t=a[i];
             }
@@ -63,25 +63,45 @@ var items=this.getItems();
 var jumptime=this.getCooldown("jump");
 var enemy=this.getNearestEnemy();
 var target=0;
+var enemyTargetClosest=getClosestTarget(items,enemy,this);
+var enemyTargetValuable=getValuableTarget(items,enemy,this);
+
 if (enemy.gold-this.gold<15) {
     target=getValuableTarget(items,this,enemy);
     } else {
     target=getClosestTarget(items,this,enemy);
-}
+    }
 
 if (jumptime===0) {
     var longJumpValue=getLongJumpValue(items,this.pos);
 }
 
-if (target!==0 && items.length>0 && (jumptime>0 || (longJumpValue[1]<=12 && longJumpValue[1]<150-this.gold)|| target.bountyGold>3)) {
-    this.move(target.pos);
-    } else if ((target!==0 && items.length>0 && jumptime===0 && (longJumpValue[1]>12 || longJumpValue>=150-this.gold))) {
-        switch (longJumpValue[0]) {
-         case "up": this.jumpTo({x:this.pos.x,y:this.pos.y+70}); break;
-         case "down": this.jumpTo({x:this.pos.x,y:this.pos.y-70}); break;
-         case "right": this.jumpTo({x:this.pos.x+70,y:this.pos.y}); break;
-         case "left": this.jumpTo({x:this.pos.x-70,y:this.pos.y}); break;
-         }
-       } else {
-             this.moveXY (60,40);
-             }
+if (jumptime===0 && ((enemyTargetClosest.bountyGold==5 && getDistance(enemyTargetClosest.pos,enemy.pos>26) && getDistance(enemyTargetClosest.pos,enemy.pos<50) && getDistance(enemyTargetClosest.pos,this.pos<70)) || (enemyTargetValuable==5 && getDistance(enemyTargetValuable.pos,enemy.pos>26) && getDistance(enemyTargetValuable.pos,enemy.pos<50) && getDistance(enemyTargetValuable.pos,this.pos<70)))) {
+            if (enemyTargetClosest.bountyGold==5) {
+                this.jumpTo(enemyTargetClosest.pos);
+            } else {
+                this.jumpTo(enemyTargetValuable.pos);
+            }
+    } else {
+        if (target!==0 && items.length>0 && (jumptime>0 || (longJumpValue[1]<=11 && longJumpValue[1]<150-this.gold)|| target.bountyGold>3)) {
+            this.move(target.pos);
+            } else if ((target!==0 && items.length>0 && jumptime===0 && (longJumpValue[1]>11 || longJumpValue>=150-this.gold))) {
+                switch (longJumpValue[0]) {
+                case "up": this.jumpTo({x:this.pos.x,y:this.pos.y+70}); break;
+                case "down": this.jumpTo({x:this.pos.x,y:this.pos.y-70}); break;
+                case "right": this.jumpTo({x:this.pos.x+70,y:this.pos.y}); break;
+                 case "left": this.jumpTo({x:this.pos.x-70,y:this.pos.y}); break;
+                }
+            } else {
+                this.moveXY (60,40);
+            }
+}
+
+
+      }
+            } else {
+                this.moveXY (60,40);
+            }
+}
+
+
